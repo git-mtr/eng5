459d17bda0414a78bb394aee98de222cd76af93a
git-mtr <122823910+git-mtr@users.noreply.github.com>
2025-07-18 18:51:10 +0200

    Create array1.js

---
+++ array1.js
@@ -0,0 +1,25 @@
+const array1 = [
+  { "en": "cause", "de": "Grund" },
+  { "en": "if", "de": "wenn" },
+  { "en": "and", "de": "und" },
+  { "en": "better", "de": "besser" },
+  { "en": "but", "de": "aber" },
+  { "en": "could", "de": "konnte" },
+  { "en": "eh", "de": "eh" },  // "eh" bleibt im Deutschen oft unverändert, je nach Kontext
+  { "en": "forever", "de": "für immer" },
+  { "en": "found", "de": "gefunden" },
+  { "en": "got", "de": "bekommen" }, // „got“ in diesem Kontext wird oft mit „bekommen“ oder „haben“ übersetzt
+  { "en": "i", "de": "ich" },
+  { "en": "in", "de": "in" },
+  { "en": "it", "de": "es" },
+  { "en": "last", "de": "dauern" },
+  { "en": "life", "de": "Leben" },
+  { "en": "lost", "de": "verloren" },
+  { "en": "never", "de": "nie" },
+  { "en": "pretended", "de": "vorgestellt" },
+  { "en": "was", "de": "war" },
+  { "en": "we", "de": "wir" },
+  { "en": "wonderland", "de": "Wunderland" },
+  { "en": "worse", "de": "schlechter" },
+  { "en": "you", "de": "du" }
+];
